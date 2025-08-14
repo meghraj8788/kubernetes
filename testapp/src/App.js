c30ca5007d93301dc830eafb2ac845417e4d744f
meghraj8788 <meghraj8788@gmail.com>
2025-08-14 10:58:10 +0530

    push

---
+++ testapp/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I am Going To be rich person in my entire blodeline:) & in worl:))))
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
